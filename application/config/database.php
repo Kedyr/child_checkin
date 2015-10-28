5979f9a24fed9d2febbf3f311599e9d5c965a642
kedyr <deniskedyr@gmail.com>
2015-10-28 23:44:27 +0300

    fixed file paths

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'username';
-$db['default']['password'] = 'password';
-$db['default']['database'] = 'database';
+$db['default']['username'] = ''; // YOUR PRODUCTION USERNAME
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
